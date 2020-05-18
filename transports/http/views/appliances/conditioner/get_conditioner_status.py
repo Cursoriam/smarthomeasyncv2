75e52d51728dbdfec6bc8abe866d48892bfb1eae
vt <coaka2013@gmail.com>
2020-05-19 00:27:46 +0200

    Fix errors

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -9,5 +9,5 @@ from services.endpoints import get_conditioner_status_data
 async def get_conditioner_status(request):
     records = await get_conditioner_status_data(request)
 
-    return web.Response(text='hello')
+    return web.json_response(records)
 
