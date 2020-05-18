8e08266589bd666ae58ed5abef39e39e7ee31435
vt <coaka2013@gmail.com>
2020-05-18 19:51:38 +0200

    change gunicorn

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -7,4 +7,4 @@ from services.endpoints import get_conditioner_status_data
 
 async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
     records = await get_conditioner_status_data(request)
-    return web.json_response(data={'hello': 'world'})
+    return web.Response(text='hello')
