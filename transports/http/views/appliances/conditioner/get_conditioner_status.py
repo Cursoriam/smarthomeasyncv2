dc2d74ef19394b49f89a6ac524325af2600280e2
vt <coaka2013@gmail.com>
2020-05-19 00:09:44 +0200

    Add request

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -6,7 +6,7 @@ from aiohttp import web
 from services.endpoints import get_conditioner_status_data
 
 
-async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
+async def get_conditioner_status(request) -> aiohttp.web.Response:
     records = await get_conditioner_status_data(request)
     try:
         time.sleep(3)
