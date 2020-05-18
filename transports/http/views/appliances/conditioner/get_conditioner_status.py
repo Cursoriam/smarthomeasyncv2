be1fc08af5a98fbec41018cdf1709d8b493d8e89
vt <coaka2013@gmail.com>
2020-05-18 18:45:04 +0200

    add data

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -6,5 +6,5 @@ from services.endpoints import get_conditioner_status_data
 
 
 async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    records = await get_conditioner_status_data()
-    return web.json_response(records)
+    records = await get_conditioner_status_data(request)
+    return web.json_response(data={'hello': 'world'})
