176c80d3dec677eb8d3fcd59ba75860b82f5953e
vt <coaka2013@gmail.com>
2020-05-18 23:52:33 +0200

    Add error handling

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -6,4 +6,7 @@ from services.endpoints import get_conditioner_status_data
 
 async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
     records = await get_conditioner_status_data(request)
-    return web.Response(text='hello')
+    try:
+        return web.Response(text='hello')
+    except Exception as ex:
+        raise ex
