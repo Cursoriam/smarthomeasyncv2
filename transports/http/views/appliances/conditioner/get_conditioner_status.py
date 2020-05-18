19fdd16f62bdb4a63716cf2cef31898e42e568ef
vt <coaka2013@gmail.com>
2020-05-19 00:07:43 +0200

    Add sleep

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -1,3 +1,5 @@
+import time
+
 import aiohttp
 from aiohttp import web
 
@@ -7,6 +9,7 @@ from services.endpoints import get_conditioner_status_data
 async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
     records = await get_conditioner_status_data(request)
     try:
+        time.sleep(3)
         return web.Response(text='hello')
     except Exception as ex:
         raise ex
