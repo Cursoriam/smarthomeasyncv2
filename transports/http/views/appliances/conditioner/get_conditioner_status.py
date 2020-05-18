e78e12eb8afaacbbd9cee822c31e229cc8f7a92d
vt <coaka2013@gmail.com>
2020-05-18 23:59:08 +0200

    Debug

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -5,7 +5,7 @@ from services.endpoints import get_conditioner_status_data
 
 
 async def get_conditioner_status(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    records = await get_conditioner_status_data(request)
+    # records = await get_conditioner_status_data(request)
     try:
         return web.Response(text='hello')
     except Exception as ex:
