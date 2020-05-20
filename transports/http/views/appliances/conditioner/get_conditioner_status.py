a17f59ae9959ae230a65a439ed351d5801d6f05e
vt <coaka2013@gmail.com>
2020-05-20 17:22:54 +0200

    Rewrite Procfile

--- transports/http/views/appliances/conditioner/get_conditioner_status.py
+++ transports/http/views/appliances/conditioner/get_conditioner_status.py
@@ -6,6 +6,6 @@ from services.endpoints import get_conditioner_status_data
 async def get_conditioner_status(request: web.Request) -> web.Response:
     id = request.query.get('id')
     records = await get_conditioner_status_data(id)
-    records = {'id': records[1], 'status': records[3], 'temperature': records[4], 'mode': records[5]}
+    records = {'id': records[0][1], 'status': records[0][3], 'temperature': records[0][4], 'mode': records[0][5]}
     return web.json_response(records)
 
